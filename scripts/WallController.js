a695fd16b4dd16c41280107ee78f8f9e1c89c4c2
Square Studio <ss@squarestudio.org>
2016-11-17 10:58:38 +0000

    Upload: scripts/WallController.js

---
+++ scripts/WallController.js
@@ -0,0 +1,134 @@
+window.Template.Controllers.WallController = function (element) {
+    'use strict';
+    var animOnScroll;
+
+    function simulateResize() {
+        window.top.innerWidth = window.top.innerWidth - 1;
+        Y.one(window.top).simulate('resize');
+        window.top.innerWidth = window.top.innerWidth + 1;
+    }
+
+    function loadImages() {
+        var images = document.querySelectorAll('#grid img[data-src]:not(.swiper-lazy)');
+        for (var i = 0; i < images.length; i++) {
+            images[i].removeAttribute('data-load');
+            ImageLoader.load(images[i], {load: true});
+        }
+    }
+
+    function initGalleries() {
+        var swipers = document.querySelectorAll('#grid .swiper-container');
+        for (var i = 0; i < swipers.length; i++) {
+            var swiper_el = $(swipers[i]);
+            swiper_el.css('height', swiper_el.outerHeight(true));
+            var swiper = new window.Swiper(swipers[i], {
+                loop: true,
+                speed: 500,
+                autoplay: 300,
+                effect: 'fade',
+                simulateTouch: false,
+                noSwiping: true,
+                onInit: function (swiper) {
+                    if(document.querySelector('html').className.indexOf('touch-styles') < 0){
+                        swiper.stopAutoplay();
+                        swiper.container[0].onmouseenter = function () {
+                            swiper.startAutoplay();
+                        };
+                        swiper.container[0].onmouseleave = function () {
+                            swiper.stopAutoplay();
+                        };
+                    }
+                }
+            });
+        }
+    }
+
+    function initVideos() {
+        if (Y.Squarespace.VideoLoader) Y.Squarespace.VideoLoader.prototype._showOverlayOnOthers = function () {
+            return false
+        };
+        var videos = Y.all('.grid-slide-video-autoplay');
+        videos.each(function (video) {
+                var videoloader = video.one('.sqs-video-wrapper').videoloader;
+                video.on('hover', function () {
+                    if (videoloader && videoloader.get('showingVideo')) {
+                        videoloader.play();
+                    } else {
+                       videoloader && videoloader.showVideo();
+                    }
+                }, function () {
+                    videoloader && videoloader.pause();
+                })
+        })
+    }
+
+    function initTexts() {
+        $('.grid-gallery-text.animated').each(function () {
+            var texts = $(this).find('.grid-slide-description-text .html-block .sqs-block-content').children().toArray();
+            var readyTexts = [];
+            var buffer_text = '';
+            texts.forEach(function (text) {
+                text = text.outerHTML;
+                if (text == '<p>--animate--</p>') {
+                    readyTexts.push(buffer_text);
+                    buffer_text = '';
+                } else if (text == '<p>--end--</p>') {
+                    readyTexts.push(buffer_text);
+                    buffer_text = '';
+                }
+                else {
+                    if (text !== '<p>&nbsp;</p>'){
+                        buffer_text += text;
+                    }
+                }
+            });
+            var longest_text = readyTexts.sort(function (a, b) { return b.length - a.length; })[0];
+            $(this).find(".text-spacer-element").html(longest_text);
+            var body_text = $(this).find(".grid-slide-description-body");
+            body_text.typed({
+                strings: readyTexts,
+                typeSpeed: 40,
+                backDelay: 1000,
+                startDelay: 700,
+                backSpeed: 10,
+                loop: true,
+                contentType: 'html',
+                showCursor: false,
+                callback: function(t) {t.backDelay = 5000;t.startDelay = 0},
+                onStringTyped: function(t) {t.backDelay = 1000}
+            });
+        });
+    }
+
+    function initialize() {
+        window.Template.Util.initShareButtons();
+        if (animOnScroll) animOnScroll = null;
+        imagesLoaded(document.getElementById("grid"), function() {
+            initGalleries();
+            initVideos();
+            initTexts();
+            animOnScroll = new AnimOnScroll(document.getElementById("grid"), {
+                minDuration: 1,
+                maxDuration: 2,
+                viewportFactor: 0.2
+            });
+            setTimeout(function () {
+                simulateResize();
+            }, 100);
+        });
+        loadImages();
+    }
+
+    initialize();
+
+    return {
+        sync: function () {
+            initialize();
+        },
+        destroy: function () {
+
+        }
+    };
+
+};
+
