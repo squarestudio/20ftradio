dc7072031fc27da98f0bfe3aac17bd4e1cbd9356
Square Studio <ss@squarestudio.org>
2016-11-17 13:47:41 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -1,7 +1,7 @@
 window.Template.Controllers.WallController = function (element) {
     'use strict';
     var animOnScroll;
-
+    console.log('ola')
     function simulateResize() {
         window.top.innerWidth = window.top.innerWidth - 1;
         Y.one(window.top).simulate('resize');
