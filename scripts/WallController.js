b2f439b4dfaa0a1f981decaca4b6cdb843a10d05
Square Studio <ss@squarestudio.org>
2016-11-23 16:53:38 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -159,6 +159,7 @@ window.Template.Controllers.WallController = function (element) {
                     getCollectionItems(url).then(function (items) {
                         console.log(items);
                         var compiled = Y.JSONTemplate.evaluateJsonTemplate(template, items); //compile template with received data
+                        console.log(compiled)
                         if(order){
                             wallGrid.prepend(compiled);
                         } else {
