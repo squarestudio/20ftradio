af3beef6d4c0f9e1343f2e48aae262b4d1eb05e7
Square Studio <ss@squarestudio.org>
2016-11-23 14:55:31 +0000

    Upload: scripts/json-template.js

--- scripts/json-template.js
+++ scripts/json-template.js
@@ -282,6 +282,7 @@ YUI.add("squarespace-json-template", function (a) {
     }, {
         name: "date",
         func: function (b, c, e) {
+            console.log(b, c, e)
             var f = 0,
                 f = (new Date(b)).getTimezoneOffset();
             if (!a.Lang.isNumber(b)) return "Invalid date.";
