f3576442777a168a14ffd30683b09c56a6a6c407
Square Studio <ss@squarestudio.org>
2016-11-23 15:31:47 +0000

    Upload: scripts/json-template.js

--- scripts/json-template.js
+++ scripts/json-template.js
@@ -412,7 +412,7 @@ YUI.add("squarespace-json-template", function (a) {
                     return "Invalid Timezone"
                 }
                 f = (isNaN(k.getTimezoneOffset()) ? 0 : k.getTimezoneOffset()) - f
-            } else c = -parseInt(c.get("website.timeZoneOffset"), 10) / 6E4, k = (new Date).getTimezoneOffset(), f = c - k;
+            } else c = -parseInt(Static.SQUARESPACE_CONTEXT.get("website.timeZoneOffset"), 10) / 6E4, k = (new Date).getTimezoneOffset(), f = c - k;
             b = new Date(b - 6E4 * f);
             e = e.join(" ");
             console.log(b, e)
