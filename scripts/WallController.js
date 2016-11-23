51d637092377f11cf161c5a29c5142a4c5e21a95
Square Studio <ss@squarestudio.org>
2016-11-23 13:55:02 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -146,6 +146,7 @@ window.Template.Controllers.WallController = function (element) {
             }, 100);
         });
         if (Y.one('.wall-item-link')){
+            var template = 
             Y.all('.wall-item-link').each(function (link) {
                 var url = link.getAttribute('href'),
                     order = link.getAttribute('data-order');
