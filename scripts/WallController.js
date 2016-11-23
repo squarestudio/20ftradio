281971bbb708f71b0484edc15b122855d84cc0b7
Square Studio <ss@squarestudio.org>
2016-11-23 17:01:32 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -159,7 +159,7 @@ window.Template.Controllers.WallController = function (element) {
                     getCollectionItems(url).then(function (items) {
                         console.log(items);
                         var compiled = Y.JSONTemplate.evaluateJsonTemplate(template, items); //compile template with received data
-                        console.log(order)
+                        console.log(Y.Node.create(compiled))
                         if(order == 'true'){
                             var nodes = getNodesOrderedByAdded(wallGrid.all('li'));
                             wallGrid.append(nodes);
