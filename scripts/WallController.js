eab27fa3b284a87331d209c02761e2d13a12989d
Square Studio <ss@squarestudio.org>
2016-11-24 21:31:27 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -3,7 +3,7 @@ window.Template.Controllers.WallController = function (element) {
     var animOnScroll,
         wallGrid,
         castPlayer,
-        playButton = Y.one('#playButton'),
+        sitePlayer = Y.one('#playButton'),
         castContainer;
     
     function simulateResize() {
