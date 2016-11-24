cdc7f87d6b80408c1e02f6af0f9c2d55437c18bf
Square Studio <ss@squarestudio.org>
2016-11-24 11:56:34 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -214,7 +214,7 @@ window.Template.Controllers.WallController = function (element) {
             });
         };
         function onPlayerError(event){
-            castPlayer.remove();
+            castPlayer.destroy();
             console.log('loading shoutcast');
             var shoutCast = Y.Node.create('<video controls="" autoplay="" name="media"><source src="'+alternUrl+'" type="audio/mpeg"></video>');
             castContainer.append(shoutCast);
