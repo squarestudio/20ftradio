61296e6feaee412edd599a1d72545dbb86732c2f
Square Studio <ss@squarestudio.org>
2016-12-01 19:38:19 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -121,7 +121,7 @@ window.Template.Controllers.CastController = function (element) {
         var castPlayer = Y.Node.create('<video id="castPlayer" class="hidden" autoplay="1" name="media"><source src="' + shoutCastUrl + '" type="audio/mpeg"></video>');
         castContainer.append(castPlayer);
         castPlayer = castPlayer._node;
-        castPlayer = videoYoutubazing(castPlayer);
+        videoYoutubazing();
         console.log(castPlayer);
         castPlayer.addEventListener('canplaythrough', onPlayerReady);
     }
