137191deca9c2cfe835c3d6106a84c66932b208d
Square Studio <ss@squarestudio.org>
2016-12-01 09:05:02 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -2,9 +2,99 @@ window.Template.Controllers.CastController = function (element) {
     'use strict';
 
     function initialize() {
-        
+        if(Y.one('#castDiv')){
+            initCast()
+        }
     }
-    
+    function initCast() {
+        castContainer = Y.one('#castDiv');
+        var videoId = castContainer.getAttribute('data-url').split('=')[1];
+        var alternUrl = castContainer.getAttribute('data-alternative-url');
+        var volumeIcon = sitePlayer.one('#volumeButton i');
+        var volumeControl = sitePlayer.one('#volControl')
+        castContainer.one('img') && castContainer.one('img').removeAttribute('data-load') && ImageLoader.load(castContainer.one('img'), {load: true, fill: true});
+        sitePlayer.one('#playButton').on('click', function (e) {
+            e.halt();
+            var state = castPlayer.getPlayerState();
+            console.log(state)
+            if (state == YT.PlayerState.PLAYING) {
+                castPlayer.pauseVideo();
+            } else if (state == YT.PlayerState.PAUSED) {
+                castPlayer.playVideo();
+            } else {
+                castPlayer.playVideo();
+            }
+        });
+        volumeIcon.on('click', function (e) {
+            e.halt();
+            if(e.currentTarget.hasClass('icono-volumeMute')){
+                castPlayer.setVolume(50);
+                volumeControl.set('value', 50);
+                volumeIcon._node.className = 'icono-volumeMedium';
+            } else {
+                castPlayer.setVolume(0);
+                volumeControl.set('value', 0);
+                volumeIcon._node.className = 'icono-volumeMute';
+            }
+        });
+        volumeControl.on(['change', 'input'], function (e) {
+            e.halt();
+            var volume = e.currentTarget.get('value');
+            if(volume > 55){
+                volumeIcon._node.className = 'icono-volumeHigh';
+            } else if(volume<55 && volume> 20){
+                volumeIcon._node.className = 'icono-volumeMedium';
+            } else if(volume<20 && volume>0){
+                volumeIcon._node.className = 'icono-volumeLow';
+            } else {
+                volumeIcon._node.className = 'icono-volumeMute';
+            }
+            castPlayer.setVolume(volume);
+        });
+        var tag = document.createElement('script');
+        tag.src = "//www.youtube.com/iframe_api";
+        var firstScriptTag = document.getElementsByTagName('script')[0];
+        firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+        window.onYouTubeIframeAPIReady =  function () {
+            window.castPlayer = castPlayer = new YT.Player('castPlayer', {
+                height: '720',
+                width: '1280',
+                videoId: videoId,
+                playerVars: {
+                    'autoplay': 1,
+                    'controls': 0,
+                    'modestbranding': 1,
+                    'rel': 0,
+                    //'showinfo': 0,
+                    'fs': 0
+                },
+                events: {
+                    'onReady': onPlayerReady,
+                    'onStateChange': onPlayerStateChange,
+                    'onError': onPlayerError
+                }
+            });
+        };
+        function onPlayerError(event){
+            castPlayer.destroy();
+            console.log('loading shoutcast');
+            var shoutCast = Y.Node.create('<video class="hidden" autoplay="1" name="media"><source src="'+alternUrl+'" type="audio/mpeg"></video>');
+            castContainer.append(shoutCast);
+        }
+        function onPlayerReady(event) {
+            console.log('playerReady');
+            event.target.setVolume(50);
+            event.target.playVideo();
+        }
+        function onPlayerStateChange(event) {
+            if (event.data == YT.PlayerState.PLAYING) {
+                sitePlayer.addClass('playing').removeClass('paused').removeClass('stopped');
+                !castContainer.hasClass('stream-activated') && castContainer.addClass('stream-activated');
+            } else if (event.data == YT.PlayerState.PAUSED) {
+                sitePlayer.removeClass('playing').removeClass('stopped').addClass('paused');
+            }
+        }
+    }
     initialize();
 
     return {
