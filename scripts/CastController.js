1c630af0b188bcb497e0c5926c0888fa7bd6b916
Square Studio <ss@squarestudio.org>
2016-12-02 12:07:12 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -125,7 +125,7 @@ window.Template.Controllers.CastController = function (element) {
     function initShoutCast(){
         console.log('shoutcast');
         playerType = 'shoutcast';
-        castPlayer.destroy();
+        castPlayer.remove();
         castPlayer = null;
         castPlayer = Y.Node.create('<video id="castPlayer" class="hidden" autoplay="1" name="media"><source src="' + shoutCastUrl + '" type="audio/mpeg"></video>');
         castContainer.append(castPlayer);
