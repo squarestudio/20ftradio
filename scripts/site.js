20cad1564d6ecb263e08ad8bf1bc6a76a472e9d7
Square Studio <ss@squarestudio.org>
2016-12-05 10:37:06 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -3,7 +3,7 @@ Y.use('node','squarespace-gallery-ng', function(Y) {
 
   Y.on('domready', function() {
     if (window.self !== window.top) {
-      
+      window.top.Y.one('.sqs-preview-frame-content').addClass('content-loaded');
     }
     Y.all('#mobile-navigation li.folder').each(function(elem) {
       elem.on('click', function() {
