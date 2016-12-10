8810fb74e4f30b5d82ecbc311ac755d3d6826519
Square Studio <ss@squarestudio.org>
2016-12-10 17:56:37 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -305,6 +305,7 @@ window.Template.Controllers.CastController = function (element) {
                     trackName.one('span').set('text', event.title);
                     console.log(event.title);
                     trackName.addClass('scroll-track');
+                    break;
                 } else {
                     trackName.one('span').set('text', '');
                     console.log('no events');
