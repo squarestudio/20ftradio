74a6c7cb74f22c1cd2e8d074cf45a612560f6324
Square Studio <ss@squarestudio.org>
2016-12-10 20:12:10 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -69,6 +69,7 @@ window.Template.Controllers.CastController = function (element) {
             };
         } else {
             console.log("No data to init youtube");
+            onPlayerError();
         }
     }
 
