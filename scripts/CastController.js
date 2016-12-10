9ccae45c18eac6b2623245b10da9336025f129f9
Square Studio <ss@squarestudio.org>
2016-12-10 17:32:22 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -291,9 +291,9 @@ window.Template.Controllers.CastController = function (element) {
             var currentTime = new Date();
             var siteTimezoneOffset = Static.SQUARESPACE_CONTEXT.website.timeZoneOffset;
             var userTimezoneOffset = currentTime.getTimezoneOffset()*60*1000;
-            currentTime = Date.UTC(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDay(), currentTime.getHours());
+           // currentTime = Date.UTC(currentTime.getFullYear(), currentTime.getMonth(), currentTime.getDay(), currentTime.getHours());
             currentEvents.upcoming.forEach(function (event) {
-                console.log(currentTime, (event.startDate+siteTimezoneOffset), currentTime - (event.endDate+siteTimezoneOffset))
+                console.log(currentTime, new Date(event.startDate+siteTimezoneOffset), currentTime - (event.endDate+siteTimezoneOffset))
                 if (currentTime >= (event.startDate+siteTimezoneOffset) && currentTime <= (event.endDate+siteTimezoneOffset)){
                     console.log(event.title)
                 }
