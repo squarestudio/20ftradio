8462e632cd01c00d0212a076d5372dfa4d61ef37
Square Studio <ss@squarestudio.org>
2016-12-15 22:14:04 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -206,7 +206,7 @@ window.Template.Controllers.CastController = function (element) {
                 volumeIcon._node.className = 'icono-volumeMute';
             }
             if (activePlayer) {
-                players[activePlayer].setVolume(volume);
+                players[activePlayer].setVolume(volume.toString());
             }
         });
         if (videoId) {
