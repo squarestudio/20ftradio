4c95e9e4e4838edbe50ede35aae428cb390cbf7c
Square Studio <ss@squarestudio.org>
2016-12-15 23:51:02 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -548,6 +548,7 @@ Y.use('node','squarespace-gallery-ng', function(Y) {
       }
       var cloned_content = header_content.cloneNode(true);
       Y.one('.footer-h-content').append(cloned_content);
+      Y.delegate('c')
     }
   });
 }());
