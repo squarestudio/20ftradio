1589cc0d6d5de6027a6138cb721dfb7e7006df43
Square Studio <ss@squarestudio.org>
2016-12-16 11:47:34 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -348,7 +348,7 @@ window.Template.Controllers.CastController = function (element) {
                 } else {//no duration
                     youtubePlayer.playVideo();
                     console.log('no youtube data');
-                    if (mobile) {
+                    if (mobile && retry > 2) {
                         retry = 5;
                         notYoutube = true;
                         checkStreams();
