3354b810a0bc1c9f382dbd4b3cf24d0e19fc3419
Square Studio <ss@squarestudio.org>
2016-12-16 13:46:28 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -322,6 +322,7 @@ window.Template.Controllers.CastController = function (element) {
                         if (state == 3) {//buffering
                             console.log('youtube buffering', retry);
                             if(retry > 5){
+                                retry = 1;
                                 activePlayer = null;
                                 if (mobile) {
                                     notYoutube = true;
