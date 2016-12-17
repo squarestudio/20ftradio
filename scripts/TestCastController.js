acf850c631aa10078d51cdbf618bfebdc1dffaac
Square Studio <ss@squarestudio.org>
2016-12-17 15:37:49 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -48,6 +48,23 @@ window.Template.Controllers.TestCastController = function (element) {
         });
     }
     function initFBPlayer() {
+        window.fbAsyncInit = function() {
+/*
+            FB.init({
+                appId      : '{your-app-id}',
+                xfbml      : true,
+                version    : 'v2.5'
+            });
+*/
+
+            // Get Embedded Video Player API Instance
+            var my_video_player;
+            FB.Event.subscribe('xfbml.ready', function(msg) {
+                if (msg.type === 'video') {
+                    my_video_player = msg.instance;
+                }
+            });
+        };
         console.log('FB init')
     }
 
