12f9dc0ff00408e9f73a3beebcba304e37b27708
Square Studio <ss@squarestudio.org>
2016-12-17 17:16:14 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -78,7 +78,7 @@ window.Template.Controllers.TestCastController = function (element) {
         };
         fbPlayer = Y.one('#fbPlayer') || null;
         if (!fbPlayer) {
-            fbPlayer = Y.Node.create('<div id="fbPlayer" data-height="' + castContainer.get('offsetHeight') + '" class="fb-video stream-player" data-allowfullscreen="false" data-href="' + videoId + '"></div>');
+            fbPlayer = Y.Node.create('<div id="fbPlayer" data-show-text="false" data-height="' + castContainer.get('offsetHeight') + '" class="fb-video stream-player" data-allowfullscreen="false" data-href="' + videoId + '"></div>');
         }
         castContainer.prepend(fbPlayer);
         if (!window.FB) {
