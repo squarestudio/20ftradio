54e49fd349ef46d6e3d32f9c85f4101818d86f91
Square Studio <ss@squarestudio.org>
2016-12-17 16:00:27 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -72,7 +72,7 @@ window.Template.Controllers.TestCastController = function (element) {
         if (!fbPlayer) {
             fbPlayer = Y.Node.create('<div id="fbPlayer" class="fb-video" data-allowfullscreen="false" data-href="'+videoId+'"></div>');
         }
-        castContainer.append(fbPlayer);
+        castContainer.prepend(fbPlayer);
     }
 
     function initYoutubePlayer() {
