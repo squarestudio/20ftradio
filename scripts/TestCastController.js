0a3f3970a314b12c258b753ac37fed3cd4f6f57c
Square Studio <ss@squarestudio.org>
2016-12-18 15:45:11 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -253,7 +253,6 @@ window.Template.Controllers.TestCastController = function (element) {
         });
         if (!youtubeUrl) {
             youtubeReady = true;
-            retry = maxRetry;
         }
         if (!shoutCastUrl) {
             shoutCastReady = true;
