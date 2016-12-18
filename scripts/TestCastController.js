12fe9a407f81f680d63d393e1e3265c25aa5a703
Square Studio <ss@squarestudio.org>
2016-12-18 12:36:22 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -17,7 +17,7 @@ window.Template.Controllers.TestCastController = function (element) {
         userPaused,
         players = {},
         activePlayer = false,
-        checkingTime = 2000,
+        checkingTime = 3000,
         streamCheckInterval,
         youtubePlayer = null,
         shoutcastPlayer = null,
