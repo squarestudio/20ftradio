736f1d4d174c761a0566151ed518c63067616878
Square Studio <ss@squarestudio.org>
2016-12-19 14:46:03 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -463,7 +463,7 @@ window.Template.Controllers.TestCastController = function (element) {
                             }
                         } else {
                             console.log('try to load shoutcast');
-                            shoutcastPlayer.play();
+                            shoutcastPlayer.load();
                             activePlayer = false;
                             if (mobile) {
                                 notShoutcast = true;
