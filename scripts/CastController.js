9ebdc9be0e8255adcd395bb1ff2182edc599c637
Square Studio <ss@squarestudio.org>
2017-01-19 09:19:54 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -733,7 +733,7 @@ window.Template.Controllers.CastController = function (element) {
                                 content_items.past = content_items.past.concat(items.past);
                             }
                             if (items.pagination && items.pagination.nextPage) {
-                                getItems(collection_url, items.pagination.nextPageUrl.toLowerCase());
+                                getItems(items.pagination.nextPageUrl);
                             } else {
                                 resolve(content_items);
                             }
