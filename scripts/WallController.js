49435367ff354e0224a69377fd72e2197af17e41
Square Studio <ss@squarestudio.org>
2017-01-19 09:19:13 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -141,7 +141,7 @@ window.Template.Controllers.WallController = function (element) {
                                 content_items.past = content_items.past.concat(items.past);
                             }
                             if (items.pagination && items.pagination.nextPage) {
-                                getItems(collection_url, items.pagination.nextPageUrl.toLowerCase());
+                                getItems(items.pagination.nextPageUrl.toLowerCase());
                             } else {
                                 resolve(content_items);
                             }
