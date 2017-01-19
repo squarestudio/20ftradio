a912d558a1686ccd84ee1a628c432136d5b9d90e
Square Studio <ss@squarestudio.org>
2017-01-19 09:34:44 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -124,7 +124,6 @@ window.Template.Controllers.WallController = function (element) {
             var offset = '';
 
             function getItems(collection_url, offset) {
-                console.log(offset)
                 Y.Data.get({
                     url: collection_url + '?format=json',
                     data: {
