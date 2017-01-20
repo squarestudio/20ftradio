9d0d0eed55704895efd28b8e9e83891095c3b4ad
Square Studio <ss@squarestudio.org>
2017-01-20 11:13:56 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -631,7 +631,7 @@ window.Template.Controllers.CastController = function (element) {
             soundCloudPlayer.setVolume(0.5);
             setActivePlayer();
         }
-        if ((youtubeReady || shoutCastReady) && retry < maxRetry) {
+        if ((youtubeReady || shoutCastReady) && retry < maxRetry  || (youtubeReady && notShoutcast)) {
             !castContainer.hasClass && castContainer.addClass('initialized');//checkStreams
             if (!streamCheckInterval) {
                 streamCheckInterval = setInterval(function () {
