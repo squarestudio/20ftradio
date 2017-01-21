0c2497f24bd6392dd0cbc8b525c4fd3ca0d2f733
Square Studio <ss@squarestudio.org>
2017-01-21 17:51:52 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -886,6 +886,7 @@ window.Template.Controllers.CastController = function (element) {
                     if (data.status == 200 && data.readyState == 4) {
                        var html = data.responseText.replace(/src=/g, 'data-href=');
                         var status_html = Y.Node.create(html);
+                        console.log(status_html.one('.newscontent table[cellpadding=4] tr:last-child .streamdata'))
                         var current_song = status_html.one('.newscontent table[cellpadding=4] tr:last-child .streamdata').get('text');
                         current_song = 'Now playing: ' + current_song;
                         console.log(current_song);
