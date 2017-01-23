a328cd875cbc538604182defbf1058ac787c31d1
Square Studio <ss@squarestudio.org>
2017-01-23 11:56:02 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -601,7 +601,7 @@ window.Template.Controllers.TestCastController = function (element) {
             mixCloudPlayer.play && mixCloudPlayer.play();
         } else {
             if (!mixCloudReady) {
-                var promise = Mixcloud.FooterWidget(someCloudUrl, {disableUnloadWarning : true, disableHotkeys: true});
+                var promise = Mixcloud.FooterWidget(someCloudUrl, {disableUnloadWarning : true, disableHotkeys: false});
                 promise.then(function(widget) {
                     mixCloudPlayer = widget;
                     mixCloudPlayer.events.play.on(function () {
