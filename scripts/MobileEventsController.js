fa060b680957085fda1d3b91eb764bc189968f7c
Square Studio <ss@squarestudio.org>
2017-01-25 20:27:08 +0000

    Upload: scripts/MobileEventsController.js

--- scripts/MobileEventsController.js
+++ scripts/MobileEventsController.js
@@ -1,116 +1,7 @@
 window.Template.Controllers.MobileEventsController = function (element) {
     'use strict';
     var animOnScroll;
-    var wallGrid = Y.one('#wallGrid');
-
-    function loadImages() {
-        var images = document.querySelectorAll('#wallGrid img[data-src]:not(.swiper-lazy)');
-        for (var i = 0; i < images.length; i++) {
-            images[i].removeAttribute('data-load');
-            ImageLoader.load(images[i], {load: true});
-        }
-    }
-
-    function initGalleries() {
-        var swipers = document.querySelectorAll('#wallGrid .swiper-container');
-        for (var i = 0; i < swipers.length; i++) {
-            var swiper_el = $(swipers[i]);
-            swiper_el.css('height', swiper_el.outerHeight(true));
-            var swiper = new window.Swiper(swipers[i], {
-                loop: true,
-                speed: 500,
-                autoplay: 300,
-                effect: 'fade',
-                simulateTouch: false,
-                noSwiping: true,
-                onInit: function (swiper) {
-                    if (document.querySelector('html').className.indexOf('touch-styles') < 0) {
-                        swiper.stopAutoplay();
-                        Y.one(swiper.container[0]).ancestor('.grid-slide')._node.onmouseenter = function () {
-                            swiper.startAutoplay();
-                        };
-                        Y.one(swiper.container[0]).ancestor('.grid-slide')._node.onmouseleave = function () {
-                            swiper.stopAutoplay();
-                        };
-                    }
-                }
-            });
-        }
-    }
-
-    function initVideos() {
-        if (Y.Squarespace.VideoLoader) Y.Squarespace.VideoLoader.prototype._showOverlayOnOthers = function () {
-            return false
-        };
-        var videos = wallGrid.all('.grid-slide-video-autoplay');
-        videos.each(function (video) {
-            var videoloader = video.one('.sqs-video-wrapper').videoloader;
-            video.on('hover', function () {
-                if (videoloader && videoloader.get('showingVideo')) {
-                    videoloader.play();
-                } else {
-                    videoloader && videoloader.showVideo();
-                }
-            }, function () {
-                videoloader && videoloader.pause();
-            })
-        });
-        var embed_videos = wallGrid.all('.sqs-video-wrapper');
-        embed_videos.each(function (embed) {
-            if (embed.getAttribute('data-html').indexOf('<iframe') > -1) {
-                var videoloader = embed.videoloader;
-                //videoloader && videoloader.showVideo();
-                if (embed.one('.intrinsic-inner')) {
-                    embed.one('.intrinsic-inner').addClass('aspect-16-9');
-                }
-            }
-        })
-    }
-
-    function initTexts() {
-        $('.grid-gallery-text.animated').each(function () {
-            var texts = $(this).find('.grid-slide-description-text .html-block .sqs-block-content').children().toArray();
-            var readyTexts = [];
-            var buffer_text = '';
-            texts.forEach(function (text) {
-                text = text.outerHTML;
-                if (text == '<p>--animate--</p>') {
-                    readyTexts.push(buffer_text);
-                    buffer_text = '';
-                } else if (text == '<p>--end--</p>') {
-                    readyTexts.push(buffer_text);
-                    buffer_text = '';
-                }
-                else {
-                    if (text !== '<p>&nbsp;</p>') {
-                        buffer_text += text;
-                    }
-                }
-            });
-            var longest_text = readyTexts.sort(function (a, b) {
-                return b.length - a.length;
-            })[0];
-            $(this).find(".text-spacer-element").html(longest_text);
-            var body_text = $(this).find(".grid-slide-description-body");
-            body_text.typed({
-                strings: readyTexts,
-                typeSpeed: 40,
-                backDelay: 1000,
-                startDelay: 700,
-                backSpeed: 10,
-                loop: true,
-                contentType: 'html',
-                showCursor: false,
-                callback: function (t) {
-                    t.backDelay = 5000;
-                    t.startDelay = 0
-                },
-                onStringTyped: function (t) {
-                    t.backDelay = 1000
-                }
-            });
-        });
-    }
+    var mobileEvents = Y.one('#mobileEvents');
 
     function getCollectionItems(collection_url) {
         return new Y.Promise(function (resolve) {
@@ -155,9 +46,9 @@ window.Template.Controllers.MobileEventsController = function (element) {
 
     function initialize() {
         console.log('Wall init');
-        wallGrid = Y.one('#wallGrid');
-        if (wallGrid) {
-            var mobileWall = wallGrid.one('.mobile-only');
+        mobileEvents = Y.one('#wallGrid');
+        if (mobileEvents) {
+            var mobileWall = mobileEvents.one('.mobile-only');
             window.Template.Util.initShareButtons();
             if (animOnScroll) animOnScroll = null;
             var imagesReady = function () {
@@ -179,7 +70,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
             if (Y.one('.wall-item-link')) {
                 Y.use(['node', 'squarespace-json-template'], function (Y) {
                     var template = Y.one(Y.one('.wall-item-link').getData('template')).getHTML().replace(/\^/g, '{');
-                    wallGrid.all('.wall-item-link').each(function (link) {
+                    mobileEvents.all('.wall-item-link').each(function (link) {
                         var url = link.getAttribute('href'),
                             order = link.getAttribute('data-first-order');
                         getCollectionItems(url).then(function (items) {
@@ -205,7 +96,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
                                     mobileWall.append(pastMob.get('children'));
                                 }
                                 var events = Y.Node.create('<ul class="wallGrid wallEvents"></ul>');
-                                wallGrid.prepend(events.prepend(compiledFragment.all('li')));
+                                mobileEvents.prepend(events.prepend(compiledFragment.all('li')));
                                 link.remove();
                                 imagesReady();
                                 loadImages();
