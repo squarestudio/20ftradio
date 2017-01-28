5587ec2a69556094edb93a62d6f75736666c80d7
Square Studio <ss@squarestudio.org>
2017-01-28 22:20:16 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -30,7 +30,7 @@ window.Template.Controllers.CastController = function (element) {
         players = {},
         activePlayer = false,
         checkingTime = 2000,
-        streamCheckInterval,
+        streamCheckInterval = false,
         youtubePlayer = null,
         fbPlayer = null,
         shoutcastPlayer = null,
