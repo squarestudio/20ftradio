1438bf3feeed68aab651a28b941bd9a6ee6a0c67
Square Studio <ss@squarestudio.org>
2017-01-28 21:29:03 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -501,7 +501,7 @@ window.Template.Controllers.CastController = function (element) {
         };
         if (!userPaused && activePlayer !== 'facebook') {
             console.log('CHECK Before Youtube');
-            if (youtubeStatusLoad) {
+            if (youtubeStatusLoad){
                 if (youtubePlayer && youtubeStatus) {
                     var state = youtubePlayer.getPlayerState && youtubePlayer.getPlayerState();
                     if (youtubeStatus) {
@@ -782,10 +782,8 @@ window.Template.Controllers.CastController = function (element) {
                 setActivePlayer();
             }
         }
-        console.log('streamCheckIntervalstreamCheckInterval', streamCheckInterval)
         if ((youtubeReady || shoutCastReady) && retry < maxRetry || (youtubeReady && notShoutcast) || notShoutcast && notYoutube && soundCloudReady) {
             !castContainer.hasClass && castContainer.addClass('initialized');//checkStreams
-            console.log('streamCheckIntervalstreamCheckInterval', streamCheckInterval)
             if (!streamCheckInterval) {
                 streamCheckInterval = setInterval(function () {
                     checkStreams();
@@ -989,7 +987,7 @@ window.Template.Controllers.CastController = function (element) {
     }
 
     function checkTrackNameOverflow() {
-        if (trackName.one('span').get('offsetWidth') > trackName.get('offsetWidth')) {
+        if(trackName.one('span').get('offsetWidth') > trackName.get('offsetWidth')){
             trackName.addClass('scrolling');
         } else {
             trackName.removeClass('scrolling');
@@ -1003,7 +1001,7 @@ window.Template.Controllers.CastController = function (element) {
                     if (data.status == 200 && data.readyState == 4) {
                         var html = data.responseText.replace(/src=/g, 'data-href=');
                         var status_html = Y.Node.create(html);
-                        if (status_html && status_html.one('.newscontent table[cellpadding=4]')) {
+                        if (status_html && status_html.one('.newscontent table[cellpadding=4]')){
                             var current_song = status_html.one('.newscontent table[cellpadding=4] tr:last-child td:last-child').get('text');
                             current_song = 'Now playing: ' + current_song;
                             console.log(current_song);
