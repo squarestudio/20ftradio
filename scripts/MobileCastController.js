9974551ff0536a88ed58c188a9b505a8f2557a4a
Square Studio <ss@squarestudio.org>
2017-01-29 20:08:57 +0000

    Upload: scripts/MobileCastController.js

--- scripts/MobileCastController.js
+++ scripts/MobileCastController.js
@@ -102,6 +102,7 @@ window.Template.Controllers.MobileCastController = function (element) {
                 'controls': 0,
                 'modestbranding': 0,
                 'rel': 0,
+                'origin': 'file://'
                 'showinfo': 0,
                 'channel': channel || '',
                 'playsinline': 1,
