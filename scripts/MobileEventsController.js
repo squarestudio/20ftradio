a6b49a9485b43d2ba4b24573d382f8eb417efce9
Square Studio <ss@squarestudio.org>
2017-01-29 15:24:58 +0000

    Upload: scripts/MobileEventsController.js

--- scripts/MobileEventsController.js
+++ scripts/MobileEventsController.js
@@ -150,7 +150,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
             var eventLocation = e.getAttribute('data-location') || "31 Nyzhnoiurkivska Street, Kyiv, Ukraine";
             var notes = e.getAttribute('data-tags') || "Listen 20FTRadio";
             window.plugins.calendar && window.plugins.calendar.findEvent(title, eventLocation, '', startDate, endDate, function (data) {
-                console.log(data)
+                console.log(data);
                 if (data.length) {
                     e.ancestor('.event-item').addClass('scheduled');
                 } else {
