847b42624695783bb5965f61511675c03f0757dc
Square Studio <ss@squarestudio.org>
2017-02-03 09:43:21 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -488,7 +488,7 @@ window.Template.Controllers.CastController = function (element) {
                         if (state == 3) {
                             youtubeRetry++;
                             if (youtubeRetry > maxRetry) {
-                                retry = maxRetry+1;
+                                retry = maxRetry+2;
                                 youtubeStatus = false;
                                 return;
                             }
