bb8ff1349add9306b3570ceba68e058ef6b8e397
Square Studio <ss@squarestudio.org>
2017-02-20 19:26:21 +0000

    Upload: scripts/ProjectController.js

--- scripts/ProjectController.js
+++ scripts/ProjectController.js
@@ -1,3 +1,23 @@
-/**
- * Created by michael on 20.02.17.
- */
+window.Template.Controllers.ProjectController = function (element) {
+    'use strict';
+
+    function initialize() {
+        if(Site && Y.one('#grid')){
+            Site.gridEl = Y.one('#grid');
+            Site._setupGrid();
+            Site._setupTweakHandler();
+        }
+    }
+
+    initialize();
+
+    return {
+        sync: function () {
+            initialize();
+        },
+        destroy: function () {
+            console.log('destroy project');
+        }
+    };
+
+};
