5923a76423e7760ec947968e2a015a313f8fbadb
Square Studio <ss@squarestudio.org>
2017-03-26 13:25:28 +0000

    Upload: scripts/json-template.js

--- scripts/json-template.js
+++ scripts/json-template.js
@@ -656,7 +656,6 @@ YUI.add("squarespace-json-template", function (a) {
             } else c = -parseInt(Static.SQUARESPACE_CONTEXT.website.timeZoneOffset, 10) / 6E4, k = (new Date).getTimezoneOffset(), f = c - k;
             b = new Date(b - 6E4 * f);
             e = e.join(" ");
-            console.log(a.DataType.Date.format(b, {format: e}))
             return a.DataType.Date.format(b, {format: e})
         }
     }, {
