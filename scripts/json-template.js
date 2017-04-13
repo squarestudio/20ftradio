564303932c0c72d7d86cf8d530f9c32e1e1adbcf
Square Studio <ss@squarestudio.org>
2017-04-13 05:48:24 +0000

    Upload: scripts/json-template.js

--- scripts/json-template.js
+++ scripts/json-template.js
@@ -7,7 +7,6 @@ Y.use(['node'], function (a) {
         d = {
             formats: {
                 a: function (a, c) {
-                    console.log(a, c)
                     return c.a[a.getDay()]
                 },
                 A: function (a, c) {
@@ -169,7 +168,6 @@ YUI.add("lang/datatype-date-format_en", function(a) {
     })
 }, "3.17.2")
 YUI.add("datatype-date-format", function (a, e) {
-    console.log(a, e)
 
 }, "3.17.2", {lang: "ar ar-JO ca ca-ES da da-DK de de-AT de-DE el el-GR en en-AU en-CA en-GB en-IE en-IN en-JO en-MY en-NZ en-PH en-SG en-US es es-AR es-BO es-CL es-CO es-EC es-ES es-MX es-PE es-PY es-US es-UY es-VE fi fi-FI fr fr-BE fr-CA fr-FR hi hi-IN hu id id-ID it it-IT ja ja-JP ko ko-KR ms ms-MY nb nb-NO nl nl-BE nl-NL pl pl-PL pt pt-BR ro ro-RO ru ru-RU sv sv-SE th th-TH tr tr-TR vi vi-VN zh-Hans zh-Hans-CN zh-Hant zh-Hant-HK zh-Hant-TW".split(" ")})
 YUI.add("squarespace-date-utils", function(a) {
