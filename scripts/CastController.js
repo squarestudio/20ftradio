aef12096352efd90d0d5748f8fedef674f7ea330
Square Studio <ss@squarestudio.org>
2017-05-30 17:21:53 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -953,7 +953,6 @@ window.Template.Controllers.CastController = function (element) {
                 trackName.one('span').set('text', eventOnAir.title);
                 trackName.addClass('scroll-track');
                 checkTrackNameOverflow();
-                console.log(eventOnAir)
                 if(Y.all('.eventlist-event').size()){
                     Y.all('.eventlist-event').each(function (item) {
                         if(item.get('innerText').indexOf(eventOnAir.fullUrl)>-1){
