f0cdb4c74364592ad726359b0f1f71c3cf4faf0e
Square Studio <ss@squarestudio.org>
2017-05-30 17:25:57 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -955,8 +955,7 @@ window.Template.Controllers.CastController = function (element) {
                 checkTrackNameOverflow();
                 if(Y.all('.eventlist-event').size()){
                     Y.all('.eventlist-event').each(function (item) {
-                        console.log(item.get('innerText'))
-                        if(item.get('innerText').indexOf(eventOnAir.fullUrl)>-1){
+                        if(item.one('a').getAttribute('href').indexOf(eventOnAir.fullUrl)>-1){
                             item.addClass('event-on-air');
                         }
                     })
