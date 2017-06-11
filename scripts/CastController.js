579a52e1bbfe2f471dd0fbec0cd26cb17136fc17
Square Studio <ss@squarestudio.org>
2017-06-11 08:00:40 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -963,7 +963,7 @@ window.Template.Controllers.CastController = function (element) {
                                 item.hide(true);
                                 setTimeout(function () {
                                     item.remove();
-                                },300)
+                                }, 300)
                             }
                         }
                     })
@@ -978,7 +978,7 @@ window.Template.Controllers.CastController = function (element) {
                     Y.one('.event-on-air').removeClass('event-on-air').hide(true);
                     setTimeout(function () {
                         Y.one('.event-on-air').remove();
-                    },300)
+                    }, 300)
                 }
             }
         };
