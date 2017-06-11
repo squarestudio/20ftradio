242cccef842b72fe419565b5a1851668e74bc1d6
Square Studio <ss@squarestudio.org>
2017-06-11 17:09:53 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -944,7 +944,7 @@ window.Template.Controllers.CastController = function (element) {
             currentTime = currentTime.getTime();
             var eventOnAir = false;
             currentEvents.upcoming.forEach(function (event) {
-                console.log(currentTime - new Date(event.endDate + siteTimezoneOffset + userTimezoneOffset).getTime())
+                console.log(currentTime - (new Date(event.endDate + siteTimezoneOffset + userTimezoneOffset).getTime()))
                 if(currentTime >= new Date(event.endDate + siteTimezoneOffset + userTimezoneOffset).getTime()){
                     Y.one('#'+event.id)&&Y.one('#'+event.id).remove();
                 }
