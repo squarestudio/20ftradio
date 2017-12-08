5d89642a2299a041f189bd0424dd212ac0134b93
Square Studio <ss@squarestudio.org>
2017-12-08 09:02:18 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -57,7 +57,7 @@ window.Template.Controllers.CastController = function (element) {
             }, 300);
             setTimeout(function () {
                 initCast();
-            }, 2800);
+            }, 800);
             Y.one(window).on('resize', refreshImages);
             if (window.self !== window.top) {
                 window.top.Y.one('.sqs-preview-frame-content').addClass('content-loaded');
