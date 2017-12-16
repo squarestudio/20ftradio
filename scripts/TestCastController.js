47a10ff67784ab6680ca3ac30b52819bfbd1da90
Square Studio <ss@squarestudio.org>
2017-12-16 11:18:51 +0000

    Upload: scripts/TestCastController.js

--- scripts/TestCastController.js
+++ scripts/TestCastController.js
@@ -662,6 +662,7 @@ window.Template.Controllers.TestCastController = function (element) {
                 });
                 onPlayerReady('mixcloud');
                 pausePlayersExept('all');
+                mixCloudFooterPlayer.play();
                 sitePlayer.addClass('played');
             });
             //players['mixcloud'] = mixCloudPlayer;
