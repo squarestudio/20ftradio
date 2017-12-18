987e6cee930e798e7cc1d51a652fee66b23f0ed0
Square Studio <ss@squarestudio.org>
2017-12-18 15:25:36 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -108,6 +108,7 @@ window.Template.Controllers.CastController = function (element) {
                 sessionStorage.setItem('20ft', 'session');
             }else {
                 $('.castWrapper').hide();
+                $('#navigator').style('padding-top', '63px');
             }
         }
     }
