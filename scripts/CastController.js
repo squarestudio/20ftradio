bdcfaac70cb4c4e8d292e1e0b6d158b98c5bf17e
Square Studio <ss@squarestudio.org>
2017-12-18 13:42:50 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -93,13 +93,14 @@ window.Template.Controllers.CastController = function (element) {
 
 
         cast = $('.castWrapper').innerHeight();
+
         if(!sessionStorage.getItem('20ft'))
         {
             sessionStorage.setItem('20ft', 'session');
         }else {
             $('html, body').animate({
                 scrollTop: cast
-            }, 300);
+            }, 1000);
         }
     }
 
