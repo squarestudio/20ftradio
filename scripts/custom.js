32c39d5b677b1832106f5d6fc8f6c1e1508206f2
Square Studio <ss@squarestudio.org>
2017-12-18 13:33:45 +0000

    Upload: scripts/custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -2,10 +2,10 @@ $(function(){
     if(!sessionStorage.getItem('20ft'))
     {
         var cast = $('.castWrapper').innerHeight();
+        sessionStorage.setItem('20ft', 'session');
+    }else {
         $('html, body').animate({
             scrollTop: cast
         }, 500);
-    }else {
-
     }
 });
