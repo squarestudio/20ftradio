32e2b543b1e733bb1ce87ed5d4a9bc92f449e315
Square Studio <ss@squarestudio.org>
2017-12-18 13:29:35 +0000

    Upload: scripts/custom.js

--- scripts/custom.js
+++ scripts/custom.js
@@ -1,10 +1,12 @@
 $(function(){
-    if(!sessionStorage.getItem('20ft'))
-    {
-        var cast = $('.castWrapper').height();
-        console.log(cast);
-    }else
-    {
+    $(window).on('load', function () {
 
-    }
+        if(!sessionStorage.getItem('20ft'))
+        {
+            var cast = $('.castWrapper').height();
+            console.log(cast);
+        }else {
+
+        }
+    });
 });
