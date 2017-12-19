1e78fa2bf077774ccf22cbd5425eb6f51a401c12
Square Studio <ss@squarestudio.org>
2017-12-19 13:05:08 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -634,6 +634,6 @@ Y.use('node', 'squarespace-gallery-ng', function (Y) {
 $(function(){
     $('.ft-donate').find('#container-content .sqs-layout .sqs-col-12 > div:nth-child(2) > div:nth-child(4) a').on('click', function (e) {
         e.preventDefault();
-        $('.ft-donate').addClass('liqpay');
+        $('.ft-donate').fadeIn();
     })
 });
