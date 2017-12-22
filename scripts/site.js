b7e170f696d7f9ec056d0424580182ac3b167f9d
Square Studio <ss@squarestudio.org>
2017-12-22 14:30:27 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -641,6 +641,6 @@ $(function(){
     });
     $('.liqpay_close').on('click', function () {
         $('#liqpay_checkout').slideUp();
-        $('#liqpay_checkout .iframe').remove();
+        $('#liqpay_checkout iframe').remove();
     });
 });
