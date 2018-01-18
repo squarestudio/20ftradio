5a69d0489f86064e3e7f97363df5f3213c6d5cef
Square Studio <ss@squarestudio.org>
2018-01-18 14:45:01 +0000

    Upload: scripts/payment.js

--- scripts/payment.js
+++ scripts/payment.js
@@ -22,11 +22,16 @@ $(function(){
         var email = $(this).val();
         localStorage.setItem('payerEmail', email);
     });
-
-    $('.ft-donate .form-block form .field-list > div:nth-last-child(2) input').on('blur', function () {
-        var address = $(this).val();
-        localStorage.setItem('payerAddress', address);
-    });
+    if(window.location.pathname === '/sticker-pack')
+    {
+        $('.ft-donate .form-block form .field-list > div:nth-last-child(2) input').on('blur', function () {
+            var address = $(this).val();
+            localStorage.setItem('payerAddress', address);
+        });
+    }else
+    {
+        localStorage.setItem('payerAddress', '');
+    }
 });
 
 function getRandomArbitrary(min, max) {
