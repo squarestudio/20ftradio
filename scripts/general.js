89a80a5042707c8d489dee1ae89d8f5eea46663e
Square Studio <ss@squarestudio.org>
2018-01-21 16:54:15 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -1,21 +1,18 @@
-if (document.querySelector('.new-events-schedule')) {
-    var new_schedule = document.querySelector('.new-events-schedule');
-    var existing_dates = [];
-    if (new_schedule.querySelector('.date-container')) {
-        new_schedule.querySelectorAll('.date-container').forEach(function (date_container) {
-            var date_attr = date_container.dataset.dateAttr;
-            if (existing_dates.indexOf(date_attr) > -1) {
-                date_container.parentNode.removeChild(date_container);
-            } else {
-                existing_dates.push(date_attr);
-                if (new_schedule.querySelector('.' + date_attr)) {
-                    var items = new_schedule.querySelectorAll('.' + date_attr);
-                    for (i = 0; i < items.length; i++) {
-                        date_container.querySelector('.items-container').appendChild(items[i])
-                    }
+Y.use('node', function () {
+    Y.on('domready', function () {
+        var move;
+        Y.all('.subnav a').each(function (a) {
+            a.on('touchstart', function () {
+                move = false;
+            });
+            a.on('touchmove', function () {
+                move = true;
+            });
+            a.on('touchend', function () {
+                if (move === false) {
+                    window.location = a.getAttribute('href');
                 }
-            }
+            });
         });
-
-    }
-}
+    });
+});
