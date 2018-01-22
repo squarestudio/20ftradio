6373e884fe3ed94fabba6de813944c45a4bd4c5d
Square Studio <ss@squarestudio.org>
2018-01-22 11:08:55 +0000

    Upload: scripts/WallController.js

--- scripts/WallController.js
+++ scripts/WallController.js
@@ -195,7 +195,7 @@ window.Template.Controllers.WallController = function (element) {
                     simulateResize();
                 }, 100);
                 var imgLoad = imagesLoaded(document.getElementById("wallGrid"));
-                var masonryRefresh = debounce(200,function() {
+                var masonryRefresh = debounce(100,function() {
                     wallGrids.each(function (grid) {
                         if(grid._node.masonry){
                             grid._node.masonry.layout();
