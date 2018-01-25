89d20b6d2acc870ba535aefcf708c10fd3b7460e
Square Studio <ss@squarestudio.org>
2018-01-25 17:14:09 +0000

    Upload: scripts/MobileEventsController.js

--- scripts/MobileEventsController.js
+++ scripts/MobileEventsController.js
@@ -2,47 +2,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
     'use strict';
     var mobileEvents = Y.one('#mobileEvents');
     var eventsTabs = mobileEvents.all('.tabs a');
-
-    function getCollectionItems(collection_url) {
-        return new Y.Promise(function (resolve) {
-            var content_items = {past: [], upcoming: []};
-            var offset = '';
-
-            function getItems(collection_url, offset) {
-                Y.Data.get({
-                    url: collection_url + '?format=json',
-                    data: {
-                        view: 'list',
-                        time: new Date().getTime(),
-                        offset: offset || ''
-                    },
-                    success: function (items) {
-                        if ((items.past && items.past.length) || (items.upcoming && items.upcoming.length)) {
-                            if (items.upcoming) {
-                                content_items.upcoming = content_items.upcoming.concat(items.upcoming);
-                            }
-                            if (items.past) {
-                                content_items.past = content_items.past.concat(items.past);
-                            }
-                            if (items.pagination && items.pagination.nextPage && offset !== items.pagination.nextPageUrl.split('offset=')[1]) {
-                                getItems(collection_url, items.pagination.nextPageUrl.split('offset=')[1]);
-                            } else {
-                                resolve(content_items);
-                            }
-                        } else {
-                            resolve(content_items);
-                        }
-                    },
-                    failure: function (e) {
-                        console.warn('error : ' + e.message);
-                        resolve(content_items);
-                    }
-                })
-            }
-
-            getItems(collection_url);
-        })
-    }
+    var tabClickEvents, scrollEvent;
 
     function initTabs() {
         Y.one('#mobileEvents .tabs').addClass('tabs-active');
@@ -50,7 +10,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
         var eventTabsContainer = mobileEvents.one('.mobileEvents-wrapper');
         var eventTabsBorder = mobileEvents.one('.tab-border');
         var eventTabsLists = mobileEvents.all('.mobileEvents');
-        eventsTabs.on('click', function (e) {
+        tabClickEvents = eventsTabs.on('click', function (e) {
             e.halt();
             eventsTabs.removeClass('active');
             e.currentTarget.addClass('active');
@@ -65,7 +25,8 @@ window.Template.Controllers.MobileEventsController = function (element) {
                 eventTabsBorder.setStyles({
                     'transform': 'translate3d(0,0,0)'
                 });
-            } else {
+            }
+            else {
                 setTimeout(function () {
                     mobileEvents.one('.mobileEvents-Upcoming').removeClass('active');
                 }, 360);
@@ -78,38 +39,48 @@ window.Template.Controllers.MobileEventsController = function (element) {
             }
             eventTabsLists.removeClass('active');
             Y.one(id).addClass('active');
-        })
+            mobileEvents.getDOMNode().className = id.replace('#', 'tab-');
+        });
+        scrollEvent = Y.one(window).on('scroll', function () {
+            if (window.pageYOffset + 100 > mobileEvents.getY()) {
+                mobileEvents.addClass('overflow-auto');
+            } else {
+                mobileEvents.removeClass('overflow-auto');
+            }
+        });
+        mobileEvents.one('.content-loader').removeAttribute('style');
     }
 
     function createEvent(e) {
         e.halt();
-        var title = e.currentTarget.getAttribute('data-title') || "Listen 20FTRadio";
-        var eventLocation = e.currentTarget.getAttribute('data-location') || "31 Nyzhnoiurkivska Street, Kyiv, Ukraine";
-        var notes = e.currentTarget.getAttribute('data-tags') || "Listen 20FTRadio";
-        var currentTime = new Date();
-        var siteTimezoneOffset = Static.SQUARESPACE_CONTEXT.website.timeZoneOffset;
-        var userTimezoneOffset = currentTime.getTimezoneOffset() * 60 * 1000;
-        var startDate = new Date(parseInt(e.currentTarget.getAttribute('data-start-date')) + siteTimezoneOffset + userTimezoneOffset); // beware: month 0 = january, 11 = december
-        var endDate = new Date(parseInt(e.currentTarget.getAttribute('data-end-date')) + siteTimezoneOffset + userTimezoneOffset);
+        if(e.currentTarget.hasClass('event-on-air')) return false;
+        var parent = e.currentTarget.ancestor('.event-item');
+        var title = parent.getAttribute('data-title') || "Listen 20FTRadio";
+        var eventLocation = parent.getAttribute('data-location') || "31 Nyzhnoiurkivska Street, Kyiv, Ukraine";
+        var notes = parent.getAttribute('data-tags') || "Listen 20FTRadio";
+        var startDate = new Date(parseInt(parent.getAttribute('data-start-date'))); // beware: month 0 = january, 11 = december
+        var endDate = new Date(parseInt(parent.getAttribute('data-end-date')));
         var calOptions = window.plugins.calendar.getCalendarOptions(); // grab the defaults
         calOptions.firstReminderMinutes = 30; // default is 60, pass in null for no reminder (alarm)
         calOptions.secondReminderMinutes = 5;
-        calOptions.url = "https://www.20ftradio.net" + e.currentTarget.getAttribute('data-url');
+        calOptions.url = "https://www.20ftradio.net" + parent.getAttribute('data-url');
         var error = function (message) {
             console.error("Error: " + message);
             setTimeout(function () {
+                Y.fire('checkScheduledEvents');
                 checkScheduledEvents();
             }, 100);
         };
         var success = function (message) {
             console.log(message);
             setTimeout(function () {
+                Y.fire('checkScheduledEvents');
                 checkScheduledEvents();
             }, 100);
         };
         var askToDelete = function (buttonIndex) {
             if (buttonIndex == 2) {
-                window.plugins.calendar && window.plugins.calendar.deleteEvent(title,eventLocation,notes,startDate,endDate,success,error);
+                window.plugins.calendar && window.plugins.calendar.deleteEvent(title, eventLocation, notes, startDate, endDate, success, error);
             }
         };
         var askToCreate = function (buttonIndex) {
@@ -117,19 +88,19 @@ window.Template.Controllers.MobileEventsController = function (element) {
                 window.plugins.calendar && window.plugins.calendar.createEventWithOptions(title, eventLocation, notes, startDate, endDate, calOptions, success, error);
             }
         };
-        if (!e.currentTarget.ancestor('.event-item').hasClass('scheduled')) {
+        if (!parent.hasClass('scheduled')) {
             navigator.notification && navigator.notification.confirm(
-                'Schedule '+title + ' show?',
+                'Schedule ' + title + ' show?',
                 askToCreate,
                 'Schedule show',
-                ['Cancel','Schedule']
+                ['Cancel', 'Schedule']
             );
         } else {
             navigator.notification && navigator.notification.confirm(
                 title + ' already scheduled.',
                 askToDelete,
                 'Delete Scheduled show',
-                ['Cancel','Delete']
+                ['Cancel', 'Delete']
             );
         }
     }
@@ -139,24 +110,20 @@ window.Template.Controllers.MobileEventsController = function (element) {
     }
 
     function checkScheduledEvents() {
-        var currentTime = new Date();
-        var siteTimezoneOffset = Static.SQUARESPACE_CONTEXT.website.timeZoneOffset;
-        var userTimezoneOffset = currentTime.getTimezoneOffset() * 60 * 1000;
-        var scheduleEvents = mobileEvents.all('.schedule-event');
-        var upcoming = mobileEvents.all('#mobile-events-upcoming li');
+        var scheduleEvents = mobileEvents.all('.event-item');
+        var upcoming = mobileEvents.all('#mobile-events-upcoming .event-item');
         var planedEvents = [];
-        if (scheduleEvents._nodes.length){
-            var startDate = new Date(parseInt(scheduleEvents.item(0).getAttribute('data-start-date')) + siteTimezoneOffset + userTimezoneOffset);
-            var endDate = new Date(parseInt(scheduleEvents.item(scheduleEvents._nodes.length-1).getAttribute('data-end-date')) + siteTimezoneOffset + userTimezoneOffset);
+        if (scheduleEvents.size()) {
+            var startDate = new Date(parseInt(scheduleEvents.item(0).getAttribute('data-start-date')));
+            var endDate = new Date(parseInt(scheduleEvents.item(scheduleEvents._nodes.length - 1).getAttribute('data-end-date')));
             window.plugins.calendar && window.plugins.calendar.findEvent(null, null, null, startDate, endDate, function (data) {
-                console.log(data);
                 if (data.length) {
                     data.forEach(function (event) {
                         scheduleEvents.each(function (e) {
                             var title = e.getAttribute('data-title') || "Listen 20FTRadio";
                             var eventLocation = e.getAttribute('data-location') || "31 Nyzhnoiurkivska Street, Kyiv, Ukraine";
-                            if (event.title == title && event.location == eventLocation){
-                                planedEvents.push(e.ancestor('.event-item'))
+                            if (event.title == title && event.location == eventLocation) {
+                                planedEvents.push(e)
                             }
                         })
                     })
@@ -174,7 +141,7 @@ window.Template.Controllers.MobileEventsController = function (element) {
     }
 
     function initEventClick() {
-        mobileEvents.all('li').on('click', function (e) {
+        mobileEvents.all('.event-item').on('click', function (e) {
             if (e.currentTarget.one('.event-descr')) {
                 e.currentTarget.all('img').each(function (img) {
                     img.removeAttribute('data-load');
@@ -188,32 +155,46 @@ window.Template.Controllers.MobileEventsController = function (element) {
     function initialize() {
         console.log('mobileEvents init');
         mobileEvents = Y.one('#mobileEvents');
-        if (mobileEvents) {
-            if (Y.one('#mobile-events-query-template')) {
-                Y.use(['node', 'squarespace-json-template'], function (Y) {
-                    var template = Y.one('#mobile-events-query-template').getHTML().replace(/\^/g, '{');
-                    var url = 'https://www.20ftradio.net/events/';
-                    getCollectionItems(url).then(function (items) {
-                        console.log(items);
-                        if (items) {
-                            var compiled = Y.JSONTemplate.evaluateJsonTemplate(template, items); //compile template with received data
-                            var compiledFragment = Y.Node.create(compiled);
-                            mobileEvents.append(compiledFragment);
-                            Y.fire('getCurrentEvent');
-                            initTabs();
-                            checkScheduledEvents();
-                            setTimeout(function () {
-                                initCalendarClick();
-                                initEventClick();
-                            }, 200);
-                        } else {
-                            mobileEvents.append(' <div class="mobileEvents-wrapper"><ul id="mobile-events-upcoming" class="mobileEvents mobileEvents-Upcoming active"><p class="no-events-message">No Results</p></ul><ul id="mobile-events-past" class="mobileEvents mobileEvents-Past"><p class="no-events-message">No Results</p></ul></div>');
-                            console.log('MobileEvents: no results');
-                            initTabs();
+        if (mobileEvents && Y.one('.mobileEvents-container')) {
+            if (Y.one('#mobile-events-upcoming')) {
+                Y.io('http://20ftradio.net/events?format=main-content', {
+                    on: {
+                        success: function (data, resp) {
+                            if (resp.responseText) {
+                                Y.one('#mobile-events-upcoming').empty().prepend(resp.responseText);
+                                Y.fire('getCurrentEvent');
+                                localizeAndBuildDates && localizeAndBuildDates();
+                                checkScheduledEvents();
+                                setTimeout(function () {
+                                    initCalendarClick();
+                                    initEventClick();
+                                }, 200);
+                            } else {
+                                console.log('MobileEvents: no results');
+                            }
+                            Y.one('#mobile-events-upcoming').show(true);
                         }
-                    })
+                    }
                 })
             }
+            if (Y.one('#mobile-events-past')) {
+                Y.io('http://20ftradio.net/shows?format=main-content', {
+                    on: {
+                        success: function (data, resp) {
+                            Y.one('#mobile-events-past').append(resp.responseText);
+                            if (Site && Y.one('#grid')) {
+                                Site.gridEl = Y.one('#grid');
+                                Site.gridEl.all('a[data-dynamic-load]').setAttribute('data-ajax-loader', 'ajax-loader-binded').removeAttribute('data-dynamic-load').removeAttribute('data-dynamic-receiver');
+                                Site._setupGrid();
+                                Site._setupTweakHandler();
+                            }
+                        }
+                    }
+                })
+            }
+            initTabs();
+        } else {
+            mobileEvents.one('.content-loader').removeAttribute('style');
         }
     }
 
@@ -225,6 +206,8 @@ window.Template.Controllers.MobileEventsController = function (element) {
         },
         destroy: function () {
             console.log('destroy Mobile Events');
+            tabClickEvents && tabClickEvents.detach();
+            scrollEvent && scrollEvent.detach();
         }
     };
 
