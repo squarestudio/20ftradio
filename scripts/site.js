f34ce8e74694424e8ae4eaf6840cd946f1947fb9
Square Studio <ss@squarestudio.org>
2018-01-25 20:05:33 +0000

    Upload: scripts/site.js

--- scripts/site.js
+++ scripts/site.js
@@ -527,24 +527,6 @@ Y.use('node', 'squarespace-gallery-ng', function (Y) {
     });
 });
 
-function donateWithLiqPay(val, name, surname, email) {
-    var callbackArr = initLiqpayCall(val, name, surname, email);
-    LiqPayCheckout.init({
-        data: callbackArr['data'],
-        signature: callbackArr['signature'],
-        embedTo: "#liqpay_checkout",
-        language: "ru",
-        mode: "embed" // embed || popup
-    }).on("liqpay.callback", function (data) {
-        console.log(data.status);
-        console.log(data);
-    }).on("liqpay.ready", function (data) {
-        // ready
-    }).on("liqpay.close", function (data) {
-        // close
-    });
-}
-
 (function () {
     'use strict';
     window.aj = new AjaxLoader({
@@ -612,7 +594,6 @@ function donateWithLiqPay(val, name, surname, email) {
             triggerWidth: 640
         });
     });
-    var formSubmitEvent = null;
     Y.config.win.Squarespace.onInitialize(Y, function () {
         var body = Y.one('body');
         Y.use('node', function () {
@@ -654,34 +635,7 @@ function donateWithLiqPay(val, name, surname, email) {
                 new_schedule.all('.event-time-wrapper').setStyles({minWidth: min_width_time + 'px'})
             }
         }
-        if (Y.one('#liqpay_checkout')) {
-            var codeBlockLiq = Y.one('#liqpay_checkout').ancestor('.code-block');
-            codeBlockLiq.addClass('hidden');
-            if (!Y.one('#liqpayAPI')) {
-                window.Y.Get.js('https://static.liqpay.ua/libjs/checkout.js', function (err, tx) {
-                    if (err) {
-                        Y.log('Error loading Lazy Summaries JS: ' + err[0].error, 'error');
-                        return;
-                    }
-                    tx && tx.nodes[0].setAttribute('id', 'liqpayAPI');
-                });
-            }
-            formSubmitEvent = Y.Global.on('form:submitSuccess', function (e) {
-                var form = Y.one('#container form');
-                var name = form.one('.first-name input').get('value') || 'John';
-                var surname = form.one('.last-name input').get('value') || 'Smith';
-                var val = form.one('.field input[placeholder*="UAH"]') ? parseInt(form.one('.field input[placeholder*="UAH"]').get('value')) : 10;
-                var email = form.one('.email input').get('value');
-                codeBlockLiq.removeClass('hidden');
-                localStorage.setItem('payerName', name);
-                localStorage.setItem('payerSurname', surname);
-                localStorage.setItem('paymentSumm', val);
-                localStorage.setItem('payerEmail', email);
-                donateWithLiqPay(val, name, surname, email);
-            });
-        }
     });
     Y.config.win.Squarespace.onDestroy(Y, function () {
-        formSubmitEvent && formSubmitEvent.detach();
     })
 }());
