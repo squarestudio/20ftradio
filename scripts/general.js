4f85126be9ee611a54285c2ae9386426615ced91
Square Studio <ss@squarestudio.org>
2018-01-30 11:20:07 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -91,7 +91,7 @@ function activateMixcloudThings() {
             var url = getParameterByName('feed', iframe.getAttribute('src'));
             widg.events.play.on(function () {
                 widg.pause();
-                mixCloudFooterPlayer&&mixCloudFooterPlayer.load&&mixCloudFooterPlayer.load(url, true);
+                mixCloudFooterPlayer&&mixCloudFooterPlayer.load&&mixCloudFooterPlayer.load(url.replace('https://mixcloud.com/'), true);
             });
             widg.events.error.on(function () {
                 console.log(url, 'error');
