9c438cf2c4e1256f016ca219065323738202192d
Square Studio <ss@squarestudio.org>
2018-01-30 08:52:24 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -84,8 +84,9 @@ function activateMixcloudThings() {
     initMixCloudFooter();
     Y.all('iframe[src*=".mixcloud"]').each(function (iframe) {
         iframe.setAttribute('data-mixcloud-play-button', iframe.getAttribute('src'));
-        var widget = Mixcloud.PlayerWidget(iframe._node,{mini:true});
+        var widget = Mixcloud.PlayerWidget(iframe._node);
         widget.ready.then(function (widg) {
+            widget.setOption('mini', true);
             var url = getParameterByName('feed', iframe.getAttribute('src'));
             widg.events.buffering.on(function () {
                 widg.pause();
