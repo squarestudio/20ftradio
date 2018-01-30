6f8716ca2881ce1b6dbafe2e5e5f377133412dc8
Square Studio <ss@squarestudio.org>
2018-01-30 08:42:39 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -757,6 +757,7 @@ window.Template.Controllers.CastController = function (element) {
         }
         if(window.mixCloudFooterPlayer&&!userPaused){
             window.mixCloudFooterPlayer.pause();
+            Y.one('body').removeClass('mixcloud-footer-playing').removeClass('mixcloud-footer-stopped');
         }
     }
 
