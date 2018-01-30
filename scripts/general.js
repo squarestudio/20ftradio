622e4e4dfa3622fdd10e6ab83f4b522c6a376a27
Square Studio <ss@squarestudio.org>
2018-01-30 14:04:36 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -103,8 +103,9 @@ function activateMixcloudThings() {
         }
         var content = item.one('.sqs-block-content');
         if (!item.hasClass('inited')) {
-            var json = code_block ? {html:} : JSON.parse(item.getAttribute('data-block-json'));
-            var feed = decodeURIComponent(json.html.split('feed=')[1].split('"')[0]).replace('https://mixcloud.com/', '').replace('https://www.mixcloud.com/', '').replace('&hide_cover=1', '') || '';
+            var json = code_block ? '' : JSON.parse(item.getAttribute('data-block-json'));
+            var feed = code_block ? decodeURIComponent(iframe_src) : decodeURIComponent(json.html.split('feed=')[1].split('"')[0]);
+            feed = feed.replace('https://mixcloud.com/', '').replace('https://www.mixcloud.com/', '').replace('&hide_cover=1', '');
             if (feed) {
                 item.setAttribute('data-mixcloud-url', '/' + feed).setAttribute('data-mixcloud-api-url', 'https://api.mixcloud.com/' + feed).addClass(slugify(feed) + '-mix-item').addClass('inited');
                 content.empty();
