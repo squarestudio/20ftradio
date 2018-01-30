39bdd913cd1242d126fa0a517906cd6a46292ca8
Square Studio <ss@squarestudio.org>
2018-01-30 14:51:05 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -170,8 +170,8 @@ if (!window_loaded && (Y.one('.embed-block[data-block-json*="mixcloud.com"]') ||
         var url = ancestor.getAttribute('data-mixcloud-url');
         ancestor.toggleClass('playing');
         if (url&&ancestor.hasClass('playing')) {
-            ancestor.addClass('current');
             Y.all('.mixcloud-item.playing:not(.current)').removeClass('playing').removeClass('current');
+            ancestor.removeClass('current');
             mixCloudFooterPlayer && mixCloudFooterPlayer.load && mixCloudFooterPlayer.load(url, true);
         } else {
             Y.all('.mixcloud-item.playing').removeClass('playing').removeClass('current');
