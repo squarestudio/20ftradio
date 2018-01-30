7974577e92456effbac3647159687d071a4c28fc
Square Studio <ss@squarestudio.org>
2018-01-30 13:22:01 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -100,8 +100,8 @@ function activateMixcloudThings() {
             embed.addClass('inited');
             $.getJSON(api_url + '?callback=', function (data) {
                 console.log("success", data);
-                content.append('<div class="custom-mixcloud-widget"><div class="track-art" style="background: #333 url('+data.pictures.medium+') no-repeat;background-size: cover"></div><div class="text-info clear">' +
-                    '<div class="play-button"></div><div class="meta"><div class="track-title">'+data.name+'</div></div></div></div>')
+                content.append('<div class="custom-mixcloud-widget"><div class="track-art" style="background: #333 url('+data.pictures.medium+') no-repeat;background-size: cover"></div><a class="text-info clear">' +
+                    '<a class="play-button" ></a><div class="meta"><div class="track-title">'+data.name+'</div></div></div></div>')
             })
                 .fail(function (err) {
                     console.log(err);
