d24673d204167d6c850a70c0c46e718c4ba9d961
Square Studio <ss@squarestudio.org>
2018-02-01 10:59:07 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -255,7 +255,7 @@ function initVisual() {
         ctx.fillStyle = 'rgb(0, 0, 0)';
         ctx.fillRect(0, 0, WIDTH, HEIGHT);
         for (var i = 0; i < bufferLength; i++) {
-            barHeight = dataArray[i] - 100;
+            barHeight = dataArray[i] - (dataArray[i]*0.5);
             //console.log(barHeight)
             var r = barHeight + (25 * (i / bufferLength));
             var g = 250 ;//* (i / bufferLength);
