3fc314dfee978d5286e940b178ebd3c6efe5f645
Square Studio <ss@squarestudio.org>
2018-02-01 09:58:49 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -234,7 +234,7 @@ function initVisual() {
             clearTimeout(id);
         };
     var analyser = context.createAnalyser();
-    analyser.fftSize = 256;
+    analyser.fftSize = 128;
     var bufferLength = analyser.frequencyBinCount;
 
     var dataArray = new Uint8Array(bufferLength);
