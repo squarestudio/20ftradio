23aec7c826510bce787925105d91a837d467ab72
Square Studio <ss@squarestudio.org>
2018-02-22 16:34:55 +0000

    Upload: scripts/mobile-app.js

--- scripts/mobile-app.js
+++ scripts/mobile-app.js
@@ -2,6 +2,8 @@ window.Squarespace.onInitialize(Y, function () {
     Y.one('body').delegate('click', function (e) {
         console.log(e);
         e.halt();
-
+        if(e.currentTarget.getAttribute('href').indexOf('/shows')>-1){
+            
+        }
     }, '[data-dynamic-load],a[href="/shows"]');
 });
