8beaa798dfa025c58deec8cca80813189b5ee7f1
Square Studio <ss@squarestudio.org>
2018-02-22 16:52:15 +0000

    Upload: scripts/mobile-app.js

--- scripts/mobile-app.js
+++ scripts/mobile-app.js
@@ -1,3 +1,31 @@
+function loadOneShow(url) {
+    if(Y.one('#mobile-events-past')){
+        Y.io(url+'?format=main-content', {
+            on: {
+                success: function (data, resp) {
+                    Y.one('#mobile-events-past').append(resp.responseText);
+                    if (Y.one('#grid')) {
+                        Site.gridEl = Y.one('#grid');
+                        Y.all('#grid img').each(function(img){ImageLoader.load(img,{load:true, fit:true})})
+                    }
+                }
+            }
+        })
+    }
+}
+function loadShows() {
+    Y.io('https://www.20ftradio.net/shows?format=main-content', {
+        on: {
+            success: function (data, resp) {
+                Y.one('#mobile-events-past').append(resp.responseText);
+                if (Y.one('#grid')) {
+                    Site.gridEl = Y.one('#grid');
+                    Y.all('#grid img').each(function(img){ImageLoader.load(img,{load:true, fit:true})})
+                }
+            }
+        }
+    })
+}
 window.Squarespace.onInitialize(Y, function () {
     Y.one('body').delegate('click', function (e) {
         e.halt();
