c7e8f0916ac0f10f36fe8dfeb47f2752b642ab1d
Square Studio <ss@squarestudio.org>
2018-02-22 16:56:17 +0000

    Upload: scripts/mobile-app.js

--- scripts/mobile-app.js
+++ scripts/mobile-app.js
@@ -1,18 +1,21 @@
 function loadOneShow(url) {
-    if(Y.one('#mobile-events-past')){
-        Y.io(url+'?format=main-content', {
+    if (Y.one('#mobile-events-past')) {
+        Y.io(url + '?format=main-content', {
             on: {
                 success: function (data, resp) {
                     Y.one('#mobile-events-past').append(resp.responseText);
                     if (Y.one('#grid')) {
                         Site.gridEl = Y.one('#grid');
-                        Y.all('#grid img').each(function(img){ImageLoader.load(img,{load:true, fit:true})})
+                        Y.all('#grid img').each(function (img) {
+                            ImageLoader.load(img, {load: true, fit: true})
+                        })
                     }
                 }
             }
         })
     }
 }
+
 function loadShows() {
     Y.io('https://www.20ftradio.net/shows?format=main-content', {
         on: {
@@ -20,23 +23,26 @@ function loadShows() {
                 Y.one('#mobile-events-past').append(resp.responseText);
                 if (Y.one('#grid')) {
                     Site.gridEl = Y.one('#grid');
-                    Y.all('#grid img').each(function(img){ImageLoader.load(img,{load:true, fit:true})})
+                    Y.all('#grid img').each(function (img) {
+                        ImageLoader.load(img, {load: true, fit: true})
+                    })
                 }
             }
         }
     })
 }
+
 window.Squarespace.onInitialize(Y, function () {
     Y.one('body').delegate('click', function (e) {
         e.halt();
         var url = e.currentTarget.getAttribute('href');
         console.log(url);
-        if(url.indexOf('/shows')>-1){
-            if(Y.one('#mobileEvents .tab-2')){
+        if (url.indexOf('/shows') > -1) {
+            if (Y.one('#mobileEvents .tab-2')) {
                 Y.one('#mobileEvents .tab-2').simulate('click');
             }
-        } else if(e.currentTarget.hasAttribute('data-dynamic-load')){
-            loadOneShow('https://www.20ftradio.net'+url);
+        } else if (e.currentTarget.hasAttribute('data-dynamic-load')) {
+            loadOneShow('https://www.20ftradio.net' + url);
         }
     }, '[data-dynamic-load],a[href="/shows"]');
 });
