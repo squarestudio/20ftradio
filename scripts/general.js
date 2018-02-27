d276e2a57fe647148d12629e84e2ccf0dc35fa5a
Square Studio <ss@squarestudio.org>
2018-02-27 07:38:04 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -175,6 +175,7 @@ Y.config.win.Squarespace.onInitialize(Y, function () {
                     Y.all('.mixcloud-item.playing:not(.current)').removeClass('playing').removeClass('current');
                     ancestor.removeClass('current');
                     mixCloudFooterPlayer && mixCloudFooterPlayer.load && mixCloudFooterPlayer.load(url, true);
+                    Y.one('html').addClass('mixcloud-footer-playing').removeClass('mixcloud-footer-stopped');
                 } else {
                     Y.all('.mixcloud-item.playing').removeClass('playing').removeClass('current');
                     mixCloudFooterPlayer && mixCloudFooterPlayer.pause && mixCloudFooterPlayer.pause();
