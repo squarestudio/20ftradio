eff8ffd7461174d7f661430a9f2558e5ba6d32a6
Square Studio <ss@squarestudio.org>
2018-02-28 11:24:48 +0000

    Upload: scripts/MobileCastController.js

--- scripts/MobileCastController.js
+++ scripts/MobileCastController.js
@@ -750,13 +750,12 @@ window.Template.Controllers.MobileCastController = function (element) {
             shoutcastPlayer.addEventListener('stalled', onShoutCastError);
             shoutcastPlayer.addEventListener('suspend', onShoutCastError);
             shoutcastPlayer.addEventListener('emptied', onShoutCastError);
-            var error
+            var currPlayed = shoutcastPlayer.played.end(0) || 0;
+            var currBuff = shoutcastPlayer.buffered.end(0) || 0;
             var onprogress = function () {
-                html = document.querySelector('html');
-                var buffered = shoutcastPlayer.buffered.end(0)||0;
-                var played = shoutcastPlayer.played.end(0)||0;
-                //console.log('PROGRESS: ',currBuff, buffered, played);
-                if(currPlayed < played && played < buffered && currBuff<buffered){
+                var buffered = shoutcastPlayer.buffered.end(0) || 0;
+                var played = shoutcastPlayer.played.end(0) || 0;
+                if (currPlayed < played && played < buffered && currBuff < buffered) {
 
                 } else {
                     //console.log('NOT PLAYING');
