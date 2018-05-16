42108e93c67ac5e34070e8867fd520303bf460d4
Square Studio <ss@squarestudio.org>
2018-05-16 09:31:16 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -290,7 +290,7 @@ Y.config.win.Squarespace.onInitialize(Y, function () {
             var activeFilterTags = Y.one('.active-filter-tags');
             var val = e.currentTarget.getAttribute('data-val');
             if(!activeFilterTags.one('[data-val]="'+val+'"')&&e.currentTarget.hasClass('active')){
-                var cloned = e.currentTarget.cloneNode();
+                var cloned = e.currentTarget.get('parentNode').cloneNode();
                 activeFilterTags.append(cloned);
             }
             filterMusicFeed();
