fc83b638f5b51c8f3e3ffd09e62ec06e81157a5b
Square Studio <ss@squarestudio.org>
2018-07-20 10:08:21 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -204,11 +204,11 @@ function filterMusicFeed() {
     });
     filterableFeed.addClass('loading');
     var sim_a = Y.Node.create('<a style="display:none" class="sim_link needsclick" data-ajax-loader="ajax-loader-binded" href="' + collectionUrl + tags_string + '"></a>');
-    Y.one('body').append(sim_a);
+    //Y.one('body').append(sim_a);
     setTimeout(function () {
-        console.log('ddd')
+        console.log('fff')
         sim_a._node.click();
-        sim_a.simulate('click');
+        //sim_a.simulate('click');
     }, 200);
 }
 
