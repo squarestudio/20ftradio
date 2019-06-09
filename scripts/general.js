8eea4fc08a0cca8836f7d820e8d776a0ef1a4ddc
Square Studio <ss@squarestudio.org>
2019-06-09 10:26:17 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -138,24 +138,7 @@ function initMixCloudFooter() {
             mixCloudFooterPlayer = widget;
             console.log('READY');
             window.mixCloudFooterPlayer = mixCloudFooterPlayer;
-            mixCloudFooterPlayer.events.play.on(function(e) {
-                console.log('ololoSH')
-                mixcloudPlay();
-            });
-            mixCloudFooterPlayer.events.pause.on(function() {
-                mixcloudPause();
-            });
-            mixCloudFooterPlayer.events.buffering.on(function(e) {
-                console.log('BUFFF')
-            });
-            mixCloudFooterPlayer.events.ended.on(function() {
-                Y.one('html').removeClass('mixcloud-footer-playing').removeClass('mixcloud-footer-stopped');
-                Y.all('.mixcloud-item.playing').removeClass('playing').removeClass('current');
-                Y.fire('mixcloud:ended');
-            });
-            mixCloudFooterPlayer.events.error.on(function(e) {
-                console.log('MixCloud Error', e);
-            });
+            
             //mixCloudFooterPlayer.load('/20ftradio/hotel-magnolia-2-w-tosha-chehonte-ross-khmil-20ft-radio-11032019/')
 
         });
