ca3fe01b9148460bab3c5f52994c9d015d21f8df
Square Studio <ss@squarestudio.org>
2019-06-09 11:01:39 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -428,7 +428,7 @@ if (!window_loaded) {
             Y.all('.mixcloud-item.playing:not(.current)').removeClass('playing').removeClass('current');
             ancestor.removeClass('current');
             if (mixCloudFooterPlayer && mixCloudFooterPlayer.load) {
-                mixCloudFooterPlayer.load(url, true)
+                mixCloudFooterPlayer.load(url, true);
                 mixCloudFooterPlayer.ready.then(function(widg) {
                     mixCloudFooterPlayer = widg;
                     console.log('LOADED', widg, widg.loaded);
