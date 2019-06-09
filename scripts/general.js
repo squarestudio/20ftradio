4370b5646c31f5193a2f532024ee5b1941c09b01
Square Studio <ss@squarestudio.org>
2019-06-09 12:37:52 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -136,9 +136,11 @@ function initMixCloudFooter() {
                 }
                 if(data.type=='event'&&data.data){
                     if(data.data.eventName == 'play'){
+                        console.log('ppp')
                         mixcloudPlay();
                     }
                      if(data.data.eventName == 'pause'){
+                        console.log('pauuuse')
                         mixcloudPause();
                     }
                 }
