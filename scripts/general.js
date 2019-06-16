36f438a4b9c090fd23be7d5ee1ee0c31584078dc
Square Studio <ss@squarestudio.org>
2019-06-16 16:28:18 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -415,7 +415,7 @@ window.Squarespace.onInitialize(Y, function() {
 });
 
 function activateFilterTags(params) {
-    console.log(params)
+    //console.log(params)
     if (params && params.tag) {
         var tags = params.tag.split(',');
         var filterItems = Y.all('.FeedFilter-item');
