d75df092cd78c4df4ee050bc11e5f29f6035264a
Square Studio <ss@squarestudio.org>
2020-03-23 12:27:55 +0000

    Upload: scripts/MobileCastController.js

--- scripts/MobileCastController.js
+++ scripts/MobileCastController.js
@@ -807,12 +807,12 @@ window.Template.Controllers.MobileCastController = function(element) {
                 shoutcastPlayer.setVolume(100);*/
                 //console.log('canplaythrough', !playedOnce, !userPaused);
                 if (!userPaused && !playedOnce) {
-                    var playPromise = shoutcastPlayer.play();
+                    /*var playPromise = shoutcastPlayer.play();
                     if (playPromise !== undefined) {
                         playPromise.then(function() {
 
                         });
-                    }
+                    }*/
                 }
             }, false);
 
