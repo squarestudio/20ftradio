8f1e0a5e7754146bf7b841502d92da5b7569eb35
Square Studio <ss@squarestudio.org>
2020-07-26 11:41:54 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -65,7 +65,7 @@ window.Template.Controllers.CastController = function(element) {
             initCast();
             Y.one(window).on('resize', refreshImages);
             if (window.self !== window.top) {
-                window.top.Y.one('.sqs-preview-frame-content').addClass('content-loaded');
+                window.top.Y.one('.sqs-site-frame').addClass('content-loaded');
             }
         }
         var currentTime = new Date();
