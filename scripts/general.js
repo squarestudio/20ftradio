1aaf61ed2930df6f7e2c0320bc62c207665255a9
Square Studio <ss@squarestudio.org>
2020-07-27 07:41:34 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -452,7 +452,7 @@ if (!window_loaded) {
             Y.all('.mixcloud-item.playing:not(.current)').removeClass('playing').removeClass('current');
             ancestor.removeClass('current');
             var currentIframe = body.one('.mixcloud-content');
-            console.log(url, ancestor, ancestor.hasClass('playing'));
+            console.log(currentIframe);
             if (!currentIframe || currentIframe && currentIframe.getAttribute('data-src') !== url) {
                 //currentIframe&&currentIframe.addClass('need-remove');
                 currentIframe = Y.Node.create('<iframe data-src="' + url + '" class="mixcloud-content" width="100%" height="60" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1autoplay=1&mini=1&disable_unload_warning=1&feed=' + (i = encodeURIComponent(decodeURIComponent(url))) + '" frameborder="0" allow="autoplay"></iframe>')
