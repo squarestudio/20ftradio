18ac01b7071d760043d3a14253723cb66ebaf827
Square Studio <ss@squarestudio.org>
2022-04-01 17:17:50 +0000

    Upload: scripts/CastController.js

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -628,7 +628,7 @@ window.Template.Controllers.CastController = function(element) {
             shoutcastStatus = true;
             shoutcastPlayer = YshoutcastPlayer._node;
             shoutcastPlayer.type = "audio/mpeg";
-            shoutcastPlayer.crossOrigin = 'anonymous';
+            //shoutcastPlayer.crossOrigin = 'anonymous';
             shoutcastPlayer.addEventListener('loadstart', function() {
                 onPlayerReady('shoutcast');
             });
