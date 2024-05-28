f740a23c6635a34923fae05b699b55c71f0c0675
Square Studio <ss@squarestudio.org>
2024-05-28 07:53:40 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -135,7 +135,7 @@ function initMixCloudFooter() {
         window.addEventListener("message", function(e) {
             var data = e.data ? JSON.parse(e.data) : false;
             if (data && data.mixcloud) {
-                console.log(data);
+                // console.log(data);
                 if (data.type && data.type == 'ready') {
                     mixCloudFooterPlayer.play && mixCloudFooterPlayer.play()
                 }
