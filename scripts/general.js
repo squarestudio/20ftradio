ae6912f006e0134a24c3759457d4133976c61d1b
Square Studio <ss@squarestudio.org>
2024-05-28 07:17:17 +0000

    Upload: scripts/general.js

--- scripts/general.js
+++ scripts/general.js
@@ -506,7 +506,10 @@ if (!window_loaded) {
                 currentIframe = Y.Node.create('<iframe data-src="' + url + '" class="mixcloud-content" width="100%" src="https://www.mixcloud.com/widget/iframe/?hide_cover=1autoplay=1&mini=0&disable_unload_warning=1&feed=' + (i = encodeURIComponent(decodeURIComponent(url))) + '" frameborder="0" allow="autoplay"></iframe>')
                 currentIframe.on('load', function() {
                     mixCloudFooterPlayer = Mixcloud.PlayerWidget(document.querySelector(".mixcloud-content"));
-                    playMixcloudFooterIfLoaded(url);
+
+                    setTimeout(function(){
+                        playMixcloudFooterIfLoaded(url);
+                    }, 5000)
                     var need_remove = mixCloudFooter.all('.need-remove');
                     if (need_remove.size()) {
                         need_remove.each(function(it) {
