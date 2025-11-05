cb94cfa4ecefda684b47bcc2484f749af810229f
Marina Ninesquares <marina@ninesquares.studio>
2025-11-05 15:34:39 +0200

    change

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -59,6 +59,15 @@ if($('body').hasClass('ft20-playground') || $('body').hasClass('ft20-default')) 
         }
     })
 
+    fetch('https://api.radiocult.fm/api/station/20ft%20Radio/schedule/live', {
+        headers: {
+            'x-api-key': 'pk_5a62b516777f48bfa17f7894a33c5361'
+        }
+    })
+        .then(res => res.json())
+        .then(data => console.log(data))
+        .catch(err => console.error(err));
+
 }
 
 window.Template.Controllers.CastController = function(element) {
