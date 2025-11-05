9e93f9ce1ca36755b5dd6dab621a0be2c3dbd521
Marina Ninesquares <marina@ninesquares.studio>
2025-11-05 14:47:56 +0200

    script connection changes

--- scripts/CastController.js
+++ scripts/CastController.js
@@ -1,14 +1,5 @@
 if($('body').hasClass('ft20-playground') || $('body').hasClass('ft20-default')) {
 
-    fetch('https://api.radiocult.fm/api/station/20ft%20Radio/schedule/live', {
-        headers: {
-            'x-api-key': 'pk_5a62b516777f48bfa17f7894a33c5361'
-        }
-    })
-        .then(res => res.json())
-        .then(data => console.log(data))
-        .catch(err => console.error(err));
-
     var grainsAudio = document.getElementById('grainsPlayer');
     var grainsPlay = document.getElementById('grainsPlay');
     var shoutcastPlay = document.getElementById('shoutcastPlay');
